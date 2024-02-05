fc42361bb8cbb0c787e2e78c99a527709dc64440
Andrey Lee <andy20042016@gmail.com>
2024-02-05 17:37:41 +0300

    testing git

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class PyLearn extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(); //test
   }
 }
