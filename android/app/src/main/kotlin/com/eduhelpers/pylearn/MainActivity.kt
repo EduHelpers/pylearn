90f461718775020b9b8fad54caf481e0ec1b3225
Arthur Fakhretdinov <aefakhretdinov_1@edu.hse.ru>
2024-02-05 16:42:35 +0300

    Uploaded all files

---
+++ android/app/src/main/kotlin/com/eduhelpers/pylearn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eduhelpers.pylearn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PyLearn());
+}
+
+class PyLearn extends StatelessWidget {
+  const PyLearn({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
